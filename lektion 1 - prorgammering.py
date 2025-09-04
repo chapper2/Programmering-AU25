8da6555ea4a7138348c9c76994f499bd1c540c3a
Casper Hornskov Hansen <chn@rybners.dk>
2025-09-04 09:32:36 +0200

    et lille forsøg

--- lektion 1 - prorgammering.py
+++ lektion 1 - prorgammering.py
@@ -8,3 +8,18 @@ def hejThomas():
     
     
 hejThomas()
+
+
+def gætThomasAlder():
+    print('Du skal gætte Thomas alder i dette spil')
+    alderen = '42'
+    gæt = input('skriv dit første gæt')
+    while(gæt != alderen):
+        if(gæt < alderen):
+            print('Dit gæt er for lavt, prøv igen')
+            gæt = input('skriv dit næste gæt')
+        elif(gæt > alderen):
+            print('Dit gæt er for højt, prøv igen')
+            gæt = input('skriv dit næste gæt')
+    else:
+        print('Tillykke du har gættet Thomas alder!')
